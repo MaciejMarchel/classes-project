c0ddba01c320c612b91ab73bb48800b431c9197f
mmarchel <20093921@mail.wit.ie>
2022-02-03 15:41:19 +0000

    first commit

---
+++ src/main/kotlin/ie/wit/classes/main.kt
@@ -0,0 +1,41 @@
+package ie.wit.classes
+
+data class Employee(var firstName: String, var lastName: String) {
+
+}
+
+private val employees = ArrayList<Employee>()
+
+fun main(args: Array<String>) {
+    employees.add(Employee("Mark", "Roche"))
+    employees.add(Employee("John", "Walsh"))
+    employees.add(Employee("Sheila", "Flynn"))
+    for ((index, value) in employees.withIndex())
+        println("$index: $value")
+
+    println("\nUsing Lambdas:\n")
+    employees.forEach  {it -> println(it)}
+}
+
+fun employeeInfo(){
+    val employee1 = Employee("Mark", "Roche")
+    val employee2 = employee1.copy(firstName = "Clare")
+    val employee3 = employee1.copy()
+
+    println(employee1)
+    println(employee2.toString())
+
+    println("employee1 hashcode = ${employee1.hashCode()}")
+    println("employee2 hashcode = ${employee2.hashCode()}")
+    println("employee3 hashcode = ${employee3.hashCode()}")
+
+    if (employee1.equals(employee2))
+        println("employee1 is equal to employee2.")
+    else
+        println("employee1 is not equal to employee2.")
+
+    if (employee1.equals(employee3))
+        println("employee1 is equal to employee3.")
+    else
+        println("employee1 is not equal to employee3.")
+}
